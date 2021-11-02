ba03963ac9acf570938c083f7d6a43b31a02ecde
EmsCode82 <emsgithub@gmail.com>
2021-11-01 19:53:01 -0500

    .

---
+++ variableList.py
@@ -0,0 +1,14 @@
+#List
+family = ["Eric", "Romy", "Dog", "Cat"]
+print(family)
+print(family[3])
+
+numbers=(45,47,265,13)
+print(numbers)
+#numbers[0]=15
+family[0]= "Daddy"
+print(family)
+
+#Dictionary
+gpas = {"Name":"Eric","GPA":3.55}
+print(gpas)
